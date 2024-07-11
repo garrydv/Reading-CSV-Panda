d15e6e0490c1183bdcd35e490f46ced32e67b83e
garrydv <igordv@icloud.com>
2024-07-10 19:04:04 -0700

    Learn Pandas

---
+++ main.py
@@ -0,0 +1,30 @@
+import pandas
+#
+#data = pandas.read_csv("weather_data.csv")
+##print(data["temp"])
+#
+#data_dict = data.to_dict()
+#print(data_dict)
+#
+#temp_list = data["temp"].to_list()
+#print(temp_list)
+#
+#data["temp"].mean()
+#
+#print(data.condition)
+
+data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_squirrels_count = len(data[data["Primary Fur Color"] == "Gray"])
+red_squirrels_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_squirrels_count = len(data[data["Primary Fur Color"] == "Black"])
+print(grey_squirrels_count)
+print(red_squirrels_count)
+print(black_squirrels_count)
+
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_squirrels_count, red_squirrels_count, black_squirrels_count]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
